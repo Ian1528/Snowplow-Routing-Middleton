c2108ea1df0872e1e94600c3115ebbf32104e5e7
Ian1528 <102779366+Ian1528@users.noreply.github.com>
2024-06-08 18:15:40 +0100

    added initialization methods

---
+++ Snowplow-Routing-Middleton/construction.py
@@ -0,0 +1,400 @@
+import networkx as nx
+import math
+import random
+import numpy as np
+from routestep import RouteStep
+from costs import single_edge_cost
+from collections import deque
+from turns import angle_between_vectors, turn_direction
+from params import DEPOT, SALT_CAP, ALPHA, SELECTION_WEIGHTS, RAND_THRESH
+from shortest_paths import predecesoors, distances, distance_matrix
+
+def visit_arc(G: nx.Graph, arc: tuple, route: list, options: bool, curr_salt : float, route_required : list[RouteStep] = [], undirected=False) -> tuple[int, int]:
+    """
+    Visits an arc and updates the current route and graph information. 
+    Returns the new node that the route is currently on and the updated salt value.
+    
+    Parameters:
+        G (networkx.Graph): The graph representing the network
+        arc (tuple): The arc to be visited
+        route (list): The partial route taken so far (default: partial_route)
+        options (bool): Flag indicating whether there were other directions for the route to take.
+        undirected (bool): Flag indicating if the graph is undirected (default: False)
+    
+    Returns:
+        int: The new node that the arc is on
+        int: The new updated value of salt
+    """
+    global EDGES_SERVICED_COUNT
+    
+    from_node = arc[0]
+    to_node = arc[1]
+    id = arc[2]
+    # initialize routstep object
+    route_step = RouteStep(from_node, to_node, id, hadOptions=options, saltval=curr_salt)
+
+    # not deadheading. So we service the arc and update the effective degree of the relevant node
+    # need a check to see if we can actually service the arc given the amount of salt we have left.
+    salt_required = G[from_node][to_node][id]['salt_per']
+    needs_servicing = G[from_node][to_node][id]['serviced']
+
+    # needs servicing and we can service it. Update weighted and effective degree
+    if needs_servicing and curr_salt >= salt_required:
+        G.nodes[from_node]['weighted_degree'] -= G[from_node][to_node][id]['priority']
+        
+        if (undirected):
+            G.nodes[to_node]['weighted_degree'] -= G[from_node][to_node][id]['priority']
+
+        curr_salt -= salt_required
+
+        G[from_node][to_node][id]['passes_rem'] -= 1 # mark that we traversed this arc
+        G[from_node][to_node][id]['serviced'] = True
+        EDGES_SERVICED_COUNT += 1
+        route_step.deadheaded = False
+
+        route_required.append(route_step)
+        
+    else:
+        G[from_node][to_node][id]['deadheading_passes'] += 1
+        route_step.deadheaded = True
+
+    route.append(route_step)
+    return to_node, curr_salt
+
+
+
+def process_node(G: nx.Graph, prev: int, curr: int) -> tuple[list[tuple[int, int, int, dict]], int, int]:
+    """
+    Identifies all required edges leaving a node.
+
+    Args:
+        G (networkX graph): The graph representing the network
+        prev (int): The previous node in the route
+        curr (int): The current node in the route
+
+    Returns:
+        list: the required edges that have the current node as the source
+        int: c_min, the smallest cost out of all required edges
+        int: c_mzx, the largest cost out of all required edges
+    """
+    c_min = math.inf
+    c_max = -math.inf
+
+    required = []
+    for edge in G.edges([curr], data=True, keys=True):
+        print(type(edge))
+        nxt = edge[1]
+        k = edge[2] # identifier for parallel edges
+        if edge[3]["serviced"] == False:
+            required.append(edge)
+            c_min = min(single_edge_cost(G, prev, curr, nxt, k), c_min)
+            c_max = max(single_edge_cost(G, prev, curr, nxt, k), c_max)
+    return required, c_min, c_max
+
+
+
+def return_to_depot(G: nx.Graph, DEPOT: int, start_node: int, route_up_to_now: list[RouteStep], route_required: list[RouteStep], salt: int, options=False) -> None:
+    """
+    Returns to the depot via the shortest path, updating any required arcs along the way. 
+
+    Args:
+        G (nx.Graph): The graph representing the current network
+        DEPOT: The depot of this graph/network
+        start_node (int): The starting point where the route is currently at
+        route_up_to_now (list[RouteStep]): A list of the full current route with deadheading included
+        route_required (list[RouteStep]): A list of the full current route without deadheading
+        salt (int): Current value of salt
+        options (bool, optional): Whether the route had options at this point. Defaults to False.
+    """
+    if len(route_up_to_now) == 0:
+        Exception("Already at depot. This should never happen")
+
+    last_step = route_up_to_now[-1]
+    lastEdge = last_step.get_edge()
+
+    path = nx.reconstruct_path(lastEdge, (DEPOT,DEPOT,0), predecessors)
+    curr = start_node
+
+    for edge in path:
+        curr, salt = visit_arc(G, curr, edge, route=route_up_to_now, route_required=route_required, options=options, curr_salt=salt)
+
+
+def has_edge_within_capacity(G: nx.Graph, node: int, curr_salt: int) -> bool:
+    """
+    Determines whether the current node has a required edge within the capacity restrictions
+    of the vehicle.
+
+    Args:
+        G (nx.Graph): graph representing the network
+        node (int): current node that is being examined
+        curr_salt (int): current amount of salt being carried
+
+    Returns:
+        bool: True if there are edges that can be serviced, False otherwise
+    """
+    if curr_salt == 0:
+        return False
+    for neighbor_edge in G.edges(node, data=True, keys=True):
+        if neighbor_edge[3]['salt_per'] <= curr_salt and neighbor_edge[3]['serviced'] == False:
+            return True
+    return False
+
+def find_nearest_required(G : nx.Graph, source: int, salt: int) -> None | list[tuple[int, int, int]]:
+    """
+    Implements a BFS to find the closest node that has a required edge the vehicle can currently service.
+
+    Args:
+        G (nx.Graph): graph representing the network
+        source (int): starting node
+        salt (int): current amount of salt being carried
+
+    Returns:
+        None: if there are no required edges in the graph that are servicable
+        list[tuple[int, int, int]]: list of edges to traverse to get to the nearest required edge
+    """
+    queue = deque()
+ 
+    # Mark the current node as visited and enqueue it
+    visited = set()
+    queue.append(source)
+    parent = dict() # store predecessors to get the nodepath
+
+    # Iterate over the queue
+    while queue:
+        # Dequeue a vertex from queue
+        current = queue.popleft()
+
+        # Get all adjacent vertices of the dequeued vertex currentNode
+        # If an adjacent has not been visited, then mark it visited and enqueue it
+        for neighbor in G.neighbors(current):
+            if neighbor not in visited:
+                # new node. Check if it has edge
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+                if neighbor not in parent:
+                    parent[neighbor] = current
+
+                if has_edge_within_capacity(G, neighbor, salt):
+                    # get the node path
+                    node_path_reversed = list()
+                    curr_node = neighbor
+                    while curr_node != source:
+                        node_path_reversed.append(curr_node)
+                        curr_node = parent[curr_node]
+                    node_path_reversed.append(source)
+                    return reversed_nodes_to_edges(G, node_path_reversed)
+    return None
+
+def reversed_nodes_to_edges(G: nx.Graph, node_path: list[int]) -> list[tuple[int, int, int]]:
+    """
+    Takes a sequences of nodes, reverses them, and returns the shortest path traversing
+    that list of reversed nodes. Helper function for find_nearest_require
+
+    Args:
+        G (nx.Graph): graph representing the network
+        node_path (list[int]): list of nodes in the path
+
+    Returns:
+        list[tuple[int, int, int]]: list of shortest edges to complete a given node path
+    """
+    edge_path = list()
+    for i in range(len(node_path)-1,0,-1):
+        node1 = node_path[i]
+        node2 = node_path[i-1]
+        # in case multiple options, pick the edge with lowest weight
+        min_time = math.inf
+        min_edge_key = None
+        for edge_key, attr in G[node1][node2].items():
+            if min_edge_key == None or attr['travel_time'] < min_time:
+                min_edge_key = edge_key
+                minTime = attr['travel_time']
+        edge_path.append((node1, node2, min_edge_key))
+
+    return edge_path
+
+
+def choose_arc(G: nx.Graph, rcl: list[tuple[int, int, dict]], prev_node: int, weights: list[float, float, float], random_threshold: float) -> list[tuple[int, int, dict]]:
+    """
+    Selects an arc from a Restricted Candidate List (RCL) based on various weights.
+
+    Parameters:
+    - G: The graph representing the road network.
+    - rcl: The Restricted Candidate List (RCL) containing the arcs to choose from.
+    - prev_node: The previous node in the path.
+    - weights: A list of weights used to calculate the arc selection probabilities. The first weight is for turn direction, the second weight is for the degree of the next node, and the third weight is for the priority of the arc.
+    - random_threshold: a float between 0 and 1 that determines the probability of choosing an arc randomly.
+    Precondition: sum of weights is 1.
+
+    Returns:
+    - The selected arc from the RCL.
+
+    Algorithm:
+    1. If the previous node is None or a random number is greater than 0.8, choose an arc randomly from the RCL.
+    2. Calculate weights for each arc in the RCL based on turn direction, degree of the next node, and priority.
+    3. Normalize the sum of the weights.
+    4. Choose an arc based on the weights, where higher weights are more likely to be chosen.
+
+    Note:
+    - The angle, turn_direction, and angle functions are assumed to be defined elsewhere.
+
+    """
+
+    assert(sum(weights) == 0)
+
+    # randomize 20% of the time
+    if (random.random() < random_threshold):
+        return random.choice(rcl)
+    
+    turn_weights = {"straight": 6, "right": 5, "left": 4, "sharp right": 3, "sharp left": 2, "u-turn": 1}
+    weights_turns = np.empty(len(rcl))
+    weights_degrees = np.empty(len(rcl))
+    weights_priority = np.empty(len(rcl))
+
+    # calculate weights by turn direction, degree of next node, and priority
+    i = 0
+    for edge in rcl:
+        curr_node = edge[0]
+        next_node = edge[1]
+        k = edge[2]
+        # only calculate turn direction if there is a previous node
+        if prev_node is not None:
+            v_x = G.nodes[curr_node]['x']-G.nodes[prev_node]['x']
+            v_y = G.nodes[curr_node]['y']-G.nodes[prev_node]['y']
+
+            w_x = G.nodes[next_node]['x']-G.nodes[curr_node]['x']
+            w_y = G.nodes[next_node]['y']-G.nodes[curr_node]['y']
+
+            v = (v_x, v_y)
+            w = (w_x, w_y)
+
+            theta = angle_between_vectors(v,w)
+
+            weights_turns[i] = (turn_weights[turn_direction(theta)])
+        weights_degrees[i] = G.nodes[next_node]['weighted_degree']
+        weights_priority[i] = G[curr_node][next_node][k]['priority']
+        i+=1
+
+    # normalize the sum of the weights
+
+    # check for division by zero. need new normalizing. This can occur if there are no required edges leaving a node
+    if np.sum(weights_degrees) != 0:
+        weights_degrees = weights_degrees / np.sum(weights_degrees)
+    if np.sum(weights_priority) != 0:
+        weights_priority = weights_priority / np.sum(weights_priority)
+
+    # check that we can calculate turn cost
+    if prev_node is not None:
+        weights_turns = weights_turns / np.sum(weights_turns)
+        weights_tot = weights[0]*weights_turns + weights[1]*weights_degrees + weights[2]*weights_priority
+    
+    # if there is no previous node, we are at depot, so turn direction doesn't matter.
+    else:
+        weights_tot = weights[1]*weights_degrees + weights[2]*weights_priority
+
+    # check if weights are all zero (which could occur if at depot and no required edges leaving the node)
+    if np.sum(weights_tot) == 0:
+        index = int(np.random.choice(np.linspace(0,len(rcl)-1,len(rcl))))
+        return rcl[index]
+
+    # normalize the weights again
+    weights_tot = weights_tot / np.sum(weights_tot)
+    
+    # choose an arc based on the weights (higher weights are more likely to be chosen)
+    index = int(np.random.choice(np.linspace(0,len(rcl)-1,len(rcl)), p=weights_tot))
+
+    return rcl[index]
+
+
+
+def RCA(G: nx.Graph, curr_node: int, route: list[RouteStep], route_required: list[RouteStep], DEPOT: int, curr_salt: float) -> tuple[list[RouteStep], list[RouteStep]]:
+    """
+    Implements the Route Construction Algorithm (RCA) for snowplow routing.
+
+    Args:
+        G (networkx.Graph): The graph representing the road network.
+        curr_node (int): The current node in the route.
+        route (list[RouteStep]): The current route.
+        route_required (list[RouteStep]): The list of already serviced edges in the current route.
+        DEPOT (int): The depot node.
+        curr_salt (float): The current salt level.
+
+    Returns:
+        tuple: A tuple containing the complete final route and the set of non-deadheaded arcs in the route.
+    """
+    
+    while True:
+        prev_node = route[-1].node1 if len(route) > 0 else None
+        
+        required_arcs, c_min, c_max = process_node(G, prev_node, curr_node)
+        rcl = [] # initialize restricted candidate list
+        
+        for edge in required_arcs:
+            cost = single_edge_cost(G, prev_node, edge[0], edge[1], edge[2])
+            salt_req = G[edge[0]][edge[1]][edge[2]]['salt_per']
+
+            if cost >= c_min and cost <= c_min + ALPHA*(c_max-c_min) and salt_req <= curr_salt:
+                rcl.append(edge)
+        
+        # chooes an arc based on restricted candidate list if not empty
+        if len(rcl) > 0:
+            multiple_neighbors = len(G.edges(curr_node)) > 1
+            chosen_arc = choose_arc(G, rcl, prev_node, SELECTION_WEIGHTS, RAND_THRESH)
+            curr_node, curr_salt = visit_arc(G, chosen_arc, options=multiple_neighbors, route=route, route_required=route_required, curr_salt=curr_salt)
+
+        # if restricted candidate list is empty, follow path to nearest node with required arc
+        else:
+            path = find_nearest_required(G, curr_node, curr_salt)
+            # no more required arcs in the graph that we can service, so we're done.
+            # return to the depot and refill salt cap
+            if path is None:
+                return_to_depot(G, curr_node, route, route_required, curr_salt, options=False)
+                return route, route_required
+            
+            # otherwise go to the arc to visit
+            for edge in path:
+                multiple_neighbors = len(G.edges(curr_node)) > 1 # could we take a different path
+                curr_node, curr_salt = visit_arc(G, edge, options=multiple_neighbors, route=route, route_required=route_required, curr_salt=curr_salt)
+                if curr_node == DEPOT:
+                    return route, route_required
+        # if we're at the depot for any reason, that's the end of a route.
+        if curr_node == DEPOT:
+            return route, route_required
+        
+
+def routeGeneration(G: nx.Graph) -> tuple[list[list[RouteStep]], list[list[RouteStep]]]:
+    """
+    Generates a full set of routes given a street network.
+    Makes a copy of the original graph to avoid modifying the original.
+
+    Args:
+        G (nx.Graph): the graph representing the street network
+
+    Returns:
+        tuple[list[list[RouteStep]], list[list[RouteStep]]]: Two lists. 
+        The first contains all instructions and connected paths, while the second contains a list of only the RouteSteps which were non-deadheading
+    """
+    global EDGES_SERVICED_COUNT
+    G_copy = G.copy()
+    curr_salt = SALT_CAP
+    curr_node = DEPOT
+    EDGES_SERVICED_COUNT = 0
+    routes = []
+    routes_only_required = []
+
+    partial_route: list[RouteStep] = list()
+    partial_route_required: list[RouteStep] = list()
+    
+    while all_serviced_fast() == False:
+        partial_route, partial_route_required = RCA(G_copy, curr_node, partial_route, partial_route_required, DEPOT, curr_salt)
+        
+        routes.append(partial_route)
+        routes_only_required.append(partial_route_required)
+        
+        curr_salt = SALT_CAP
+        curr_node = DEPOT
+
+        partial_route = list()
+        partial_route_required = list()
+    
+    return routes, routes_only_required
---
+++ Snowplow-Routing-Middleton/initialize.py
@@ -0,0 +1,215 @@
+import pandas as pd
+import osmnx as ox
+import numpy as np
+import geopandas as gpd
+import networkx as nx
+import pickle
+
+def add_toy_street_info(G: nx.Graph) -> None:
+    """
+    Adds street information to the graph, 
+    including road priorities, number of passes required, and salt requirements for each 
+    road segment. To simulate real data, a random subset of roads are made unrequired and marked.
+
+    Args:
+        G (nx.Graph): The graph to which street information will be added.
+
+    Returns:
+        None
+    """
+
+    priority_keys = {"residential":1, "tertiary":2, "motorway_link":3, "primary":4}
+    passes_keys = {"residential":1, "tertiary":1, "motorway_link":2, "primary":2}
+    salt_keys = {"residential":1, "tertiary":1, "motorway_link":2, "primary":3}
+
+    for edge in G.edges(data=True, keys=True):
+        attrb = edge[3]
+        highway_type = attrb['highway']
+        attrb['priority'] = priority_keys[highway_type]
+        if 'lanes' in attrb:
+            attrb['passes_rem'] = float(attrb['lanes']) // 2
+            attrb['salt_per'] = float(attrb['lanes']) // 2
+        else:
+            attrb['passes_rem'] = passes_keys[highway_type]
+            attrb['salt_per'] = salt_keys[highway_type]
+        attrb['serviced'] = False
+    
+    for edge in G.edges(data=True):
+        attrb = edge[2]
+        if np.random.random() < 0.1:
+            attrb['passes_rem'] = 0
+            attrb['priority'] = 0
+            attrb['salt_per'] = 0
+            attrb['serviced'] = True
+
+def config_graph_attributes(G: nx.Graph) -> None:
+    """
+    Configures the graph attributes by calculating the weighted degree for each node
+    and adding a new attribute called "deadheading_passes" to each edge.
+
+    Parameters:
+    G (nx.Graph): The input graph.
+
+    Returns:
+    None
+    """
+
+    for node in G.nodes:
+        weight_deg = 0
+        for edge in G.edges([node], data=True):
+            weight_deg += edge[2]['passes_rem'] * edge[2]['priority']
+        G.nodes[node]['weighted_degree'] = weight_deg
+
+    # add a new attribute to each edge, called "deadheading_passes" and initially set to 0
+    nx.set_edge_attributes(G, 0, "deadheading_passes")
+
+
+def create_small_toy() -> nx.MultiDiGraph:
+    """
+    Create a small toy graph for snowplow routing in Middleton.
+
+    Reads the edge and node data from CSV files and constructs a directed graph.
+    Calculates the weighted degree of each node based on the edges and their attributes.
+    Sets the weighted degree as a node attribute.
+    Sets the deadheading_passes attribute of all edges to 0.
+    Sets all edges to initially be unserviced
+    Returns:
+        G (networkx.MultiDiGraph): The constructed graph.
+
+    """
+    edgelist = pd.read_csv("Snowplow-Routing-Middleton/graph_data/edges.csv")
+    nodelist = pd.read_csv("Snowplow-Routing-Middleton/graph_data/nodes.csv")
+
+    G = nx.MultiDiGraph()
+    for i, edges in edgelist.iterrows():
+        G.add_edge(edges.iloc[0], edges.iloc[1], **edges.iloc[2:].to_dict())
+        G.add_edge(edges.iloc[1], edges.iloc[0], **edges.iloc[2:].to_dict())
+
+    for i, nodes in nodelist.iterrows():
+        attributes = nodes[1:].to_dict()
+        weight_deg = 0
+        for edge in G.edges([nodes['id']], data=True):
+            weight_deg += edge[2]['passes_rem'] * edge[2]['priority']
+
+        attributes["weighted_degree"] = weight_deg
+        nx.set_node_attributes(G, {nodes['id']: attributes})
+
+    nx.set_edge_attributes(G, 0, "deadheading_passes")
+    nx.set_edge_attributes(G, False, "serviced")
+
+    return G
+
+def create_small_streets() -> nx.MultiDiGraph:
+    """
+    Creates a small streets network graph.
+
+    Returns:
+        nx.MultiDiGraph: The small streets network graph.
+    """
+    point = (43.095273490280036, -89.50918121778929)
+    G = ox.graph_from_point(point, dist=500, network_type='drive')
+    G = ox.add_edge_speeds(G)
+    G = ox.add_edge_travel_times(G)
+
+    G = nx.convert_node_labels_to_integers(G)
+
+    scc = list(nx.strongly_connected_components(G)) # strongly connected components
+    scc.remove(max(scc, key=len))
+
+    for i in scc:
+        for j in i:
+            G.remove_node(j) # remove all but the strongest connected component from G
+
+    add_toy_street_info(G)
+    config_graph_attributes(G)
+
+    # add geometry attribute to all edges
+    nodes, edges = ox.graph_to_gdfs(G)
+    G = ox.graph_from_gdfs(nodes, edges)
+    return G
+
+def create_full_streets() -> nx.MultiDiGraph:
+    """
+    Creates a full streets network graph
+
+    Returns:
+        nx.MultiDiGraph: The full scale streets network
+    """
+    # Read the shapefile
+    street_gdf = gpd.read_file("Snowplow-Routing-Middleton/graph_data/OSMWithData.gpkg")
+
+    # Read the OSM Graph
+    G = pickle.load(open('Snowplow-Routing-Middleton/graph_data/streets_graph.pickle', 'rb'))
+    G = nx.convert_node_labels_to_integers(G)
+
+    nodes, edges = ox.graph_to_gdfs(G) # better than momepy b/c fills in missing geometry attributes
+
+    edges['jurisdiction'] = np.array(street_gdf['Jurisdicti'])
+    edges['width'] = np.array(street_gdf['With_EE_ft'])
+    edges['roadtype'] = np.array(street_gdf['abvPostTyp'])
+    edges['maintainer'] = np.array(street_gdf['Maintained'])
+
+    priority_keys = {"motorway_link":1, "tertiary_link":1, "secondary_link":1, "primary_link":1, "unclassified":1, "residential":2, "tertiary":3, "secondary":4, "primary":5, "motorway":6}
+    passes_keys = {"motorway_link":1, "tertiary_link":1, "secondary_link":1, "primary_link":1, "unclassified":1, "residential":2, "tertiary":3, "secondary":4, "primary":5, "motorway":6}
+    salt_keys = {"motorway_link":1, "tertiary_link":1, "secondary_link":1, "primary_link":1, "unclassified":1, "residential":2, "tertiary":3, "secondary":4, "primary":5, "motorway":6}
+
+    priorities = np.empty(len(edges))
+    passes = np.empty(len(edges))
+    salt = np.empty(len(edges))
+    serviced = np.empty(len(edges), dtype=bool)
+
+    # go through each edge and update dictionary
+    for row in range(len(edges)):
+        highway_type = edges.iloc[row]['highway']
+        if street_gdf.iloc[row]['Jurisdicti'] == "City":
+            priorities[row] = priority_keys[highway_type]
+            passes[row] = passes_keys[highway_type]
+            salt[row] = salt_keys[highway_type]
+            serviced[row] = False
+        else:
+            priorities[row] = 0
+            passes[row] = 0
+            salt[row] = 0
+            serviced[row] = True
+
+
+    edges['priority'] = priorities
+    edges['passes_rem'] = passes
+    edges['salt_per'] = salt
+    edges['serviced'] = serviced
+    
+    G = ox.graph_from_gdfs(nodes, edges)
+    scc = list(nx.strongly_connected_components(G)) # strongly connected components
+    scc.remove(max(scc, key=len))
+
+    for i in scc:
+        for j in i:
+            G.remove_node(j) # remove all but the strongest connected component from G
+
+    config_graph_attributes(G)
+    return G
+  
+
+def add_multiedges(G: nx.Graph) -> nx.MultiDiGraph:
+    """
+    Adds multiple edges to a graph based on the 'passes_rem' attribute of each edge.
+
+    Parameters:
+        G (nx.Graph): The input graph.
+
+    Returns:
+        nx.MultiDiGraph: The modified graph with multiple edges.
+
+    """
+    G_new = nx.MultiDiGraph(G)
+    for edge in G.edges(data=True):
+        attrbs = edge[2]
+        for i in range(int(attrbs['passes_rem']-1)):
+            G_new.add_edge(edge[0], edge[1],i+1)
+            nx.set_edge_attributes(G_new, {(edge[0], edge[1],i+1):attrbs})
+
+if __name__ == "__main__":
+    create_small_toy()
+    create_full_streets()
+    create_small_streets()
+    
